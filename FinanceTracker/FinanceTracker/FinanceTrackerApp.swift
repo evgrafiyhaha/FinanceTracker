848594d13bab87e369a3112df17db00c125cba2f
evgrafiy <evgrafiyha@gmail.com>
2025-06-07 18:23:47 +0300

    Initial commit

---
+++ FinanceTracker/FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Артемий Толкишевский on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
